4fa54eda8a6cbae59e2ceb353f4edf80371e73e2
Andrea Procopio <andrea.procopio@reddoak.com>
2024-02-29 12:49:19 +0100

    initial project configuration

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.scss';
+import Component from './components/Component';
+
+const App = () => {
+  return (
+    <>
+      <div>React weather</div>
+      <Component /> 
+    </>
+  )
+}
+
+export default App
