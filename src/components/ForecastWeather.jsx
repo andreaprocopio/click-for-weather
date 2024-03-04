d230f69c76b4092f1586011e5092ab42f418f1e8
Andrea Procopio <andrea.procopio@reddoak.com>
2024-03-05 00:19:04 +0100

    updating env variables

--- src/components/ForecastWeather.jsx
+++ src/components/ForecastWeather.jsx
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from 'react';
 import ForecastItem from './ForecastItem';
 
 const ForecastWeather = ({ weatherCords }) => {
-    const API_KEY = process.env.REACT_APP_OPEN_WEATHER_API;
+    // const API_KEY = process.env.REACT_APP_OPEN_WEATHER_API;
+    const API_KEY = "f8106cb232c7ef1f9e975f6539646292"
     const URL =  `https://api.openweathermap.org/data/2.5/forecast?lat=${weatherCords.latitude}&lon=${weatherCords.longitude}&appid=${API_KEY}&units=metric`;
 
     const [weather, setWeather] = useState([]);
